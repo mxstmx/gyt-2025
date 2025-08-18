f582b8f4d4911ac9cd832b2429ae8734ae0b2407
Max Krauss <76940808+mxstmx@users.noreply.github.com>
2025-08-18 09:34:58 +0200

    Resolve merge conflicts with main

--- wp-content/themes/most-child/functions.php
+++ wp-content/themes/most-child/functions.php
@@ -12,3 +12,29 @@ function allow_svg_upload($mimes) {
     return $mimes;
 }
 add_filter('upload_mimes', 'allow_svg_upload');
+
+add_filter( 'etn_speaker_content_template_path', 'most_child_custom_speaker_template' );
+
+function most_child_custom_speaker_template( $path ) {
+    $custom = get_stylesheet_directory() . '/templates/speaker-grid.php';
+    if ( file_exists( $custom ) ) {
+        return $custom;
+    }
+    return $path;
+}
+add_action( 'after_setup_theme', function() {
+    add_image_size( 'speaker-event', 1024, 512, true );
+} );
+
+add_action( 'init', function() {
+    foreach ( get_post_types() as $post_type ) {
+        if ( post_type_supports( $post_type, 'comments' ) ) {
+            remove_post_type_support( $post_type, 'comments' );
+            remove_post_type_support( $post_type, 'trackbacks' );
+        }
+    }
+} );
+
+add_filter( 'comments_open', '__return_false', 20, 2 );
+add_filter( 'pings_open', '__return_false', 20, 2 );
+add_filter( 'comments_array', '__return_empty_array', 10, 2 );
